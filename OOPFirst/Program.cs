4a443118cb048ed7617bb1ee131b7dae072d2cde
Opilane TTHK <opilane@tthk.ee>
2022-02-28 13:24:47 +0200

    Add project files.

---
+++ OOPFirst/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            inimene[] inimesed = new inimene[3];
+            for (int i = 0; i < 3; i++)
+            {
+                inimesed[i] = new inimene();
+                Console.WriteLine("Nimi: ");
+                inimesed[i].Eesnimi = Console.ReadLine();
+                Console.WriteLine("Vanus: ");
+                inimesed[i].Vanus = int.Parse(Console.ReadLine());
+            }
+            foreach (inimene inimene in inimesed)
+            {
+                inimene.Tervitame();
+            }
+
+
+            inimene mees = new inimene();
+            mees.Eesnimi = "Mati";
+            Console.WriteLine("Ma olen {0}, kui vana ma olen?", mees.Eesnimi);
+            mees.Vanus = int.Parse(Console.ReadLine());
+            mees.Tervitame();
+            inimene naine = new inimene("Marina", Emakeel.saksa);
+
+            Console.WriteLine(naine.Emakeel);
+            naine.Tervitame();
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ OOPFirst/inimene.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPFirst
+{
+    public class inimene
+    {
+        string eesnimi; // Поля
+        int vanus;
+        string staatus;
+        string sugu;
+        int kasv;
+        string stat;
+        Emakeel emakeel;
+
+
+
+
+
+
+
+
+        public inimene() { } // Конструктор
+        public inimene(string Eesnimi, Emakeel emakeel)
+        {
+            eesnimi = Eesnimi;
+            this.emakeel = emakeel;
+        }
+
+
+
+        //public inimene() { }
+        //public inimene(string Sugu, int Kasv)
+        //{
+        //    sugu = Sugu;
+        //    kasv = Kasv;
+        //}
+
+
+
+        public string Eesnimi
+        {
+            set
+            {
+                if (eesnimi == null) eesnimi = value;
+            }
+            get { return eesnimi; }
+        } // Св-ва
+        public int Vanus
+        {
+            set 
+            { vanus = value;
+                if (vanus<7)
+                {
+                    staatus = "Väike laps";
+                }
+                else if (vanus<17)
+                {
+                    staatus = "Kooli laps";
+                }
+                else if (vanus<65)
+                {
+                    staatus = "tööline inimene";
+                }
+                else
+                {
+                    staatus = "Senior";
+                }
+            }
+            get { return vanus; }
+        }
+        public string Staatus
+        {
+            set { staatus = value; }
+            get { return staatus; }
+        }
+
+        public string Sugu
+        {
+            set 
+            { if (sugu == null) sugu = value; }
+            get { return sugu; }
+        }
+
+        public int Kasv
+        {
+            set
+            {
+                kasv = value;
+                if (kasv < 160)
+                {
+                    stat = "Väike";
+                }
+                else if (kasv < 185)
+                {
+                    stat = "Keskmine";
+                }
+                else if (kasv < 210)
+                {
+                    stat = "Pikk";
+                }
+            }
+            get { return kasv; }
+        }
+
+
+        public void Tervitame() // Метод
+        {
+            Console.WriteLine("Tere!");
+            Console.WriteLine("Minu nimi on {0}. Olen {1} aastat vana. Minu staatus on {2}", eesnimi, vanus, staatus );
+        }
+
+        public void Pool()
+        {
+            Console.WriteLine("Tervist!");
+            Console.WriteLine("Minu kasv on {0} ja minu sugu on {1}", stat, sugu);
+        }
+    }
+}
